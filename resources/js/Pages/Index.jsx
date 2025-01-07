6c114c9a5f7e8e339e2f23e864718c73d06dfbcf
maulanaibnusaban <ibnusaban67@gmail.com>
2025-01-07 11:41:15 +0700

    landing page : component

---
+++ resources/js/Pages/Index.jsx
@@ -0,0 +1,11 @@
+import Beranda from "./landing-page/Beranda";
+import Keunggulan from "./landing-page/Keunggulan";
+
+export default function Index() {
+    return (
+        <main>
+            <Beranda />
+            <Keunggulan />
+        </main>
+    );
+}
---
+++ resources/js/Pages/landing-page/Keunggulan.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Keunggulan() {
+    return <div>Keunggulan</div>;
+}
+
+export default Keunggulan;
