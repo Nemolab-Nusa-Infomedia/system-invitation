0c813856b5643d4574d025027c06534c6d100c32
rizkitriananda <riskitriananda321@gmail.com>
2025-01-07 22:18:40 +0700

    add page: login and register

---
+++ resources/js/Components/dashboard/PrimaryButton.jsx
@@ -0,0 +1,20 @@
+export default function PrimaryButton({
+    className = "",
+    disabled,
+    children,
+    ...props
+}) {
+    return (
+        <button
+            {...props}
+            className={
+                `inline-flex items-center  border border-transparent bg-[#8B4513] px-4 py-2 font-semibold  text-white transition duration-150 ease-in-out hover:bg-[#743a10] focus:bg-[#743a10] focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 ${
+                    disabled && "opacity-25"
+                } ` + className
+            }
+            disabled={disabled}
+        >
+            {children}
+        </button>
+    );
+}
