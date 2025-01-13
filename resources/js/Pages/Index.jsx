21e603a1160dfa5af189926da94d5fe0e653f8d8
maulanaibnusaban <ibnusaban67@gmail.com>
2025-01-13 10:24:03 +0700

    add : cs

--- resources/js/Pages/Index.jsx
+++ resources/js/Pages/Index.jsx
@@ -5,10 +5,19 @@ import Bantuan from "./landing-page/Bantuan";
 import FiturPaket from "./landing-page/FiturPaket";
 import Footer from "@/Components/landing-page/Footer";
 import Template from "./landing-page/Template";
+import cs from "/public/asset-landing-page/cs.png";
 
 export default function Index() {
     return (
-        <main>
+        <main className="relative">
+            <a href="https://wa.me/+6281367738354?text=Halo" target="_blank">
+                <img
+                    src={cs}
+                    className="fixed bottom-4 right-4 z-50 w-28"
+                    alt=""
+                />
+            </a>
+
             <Navbar />
             <Beranda />
             <Keunggulan />
