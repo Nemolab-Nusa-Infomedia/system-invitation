5ec09e270f337c326215e14ef61410b9270f0e48
maulanaibnusaban <ibnusaban67@gmail.com>
2025-02-19 16:09:00 +0700

    updated

--- resources/js/Pages/Invitation/Invitation.jsx
+++ resources/js/Pages/Invitation/Invitation.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 // import WeddingV1 from "./Template/Wedding/WeddingV1";
-// import KhitanV1 from "./Template/Khitan/KhitanV1";
-import BottomNavbar from "@/Components/invitation/BottomNavbar";
 import NatalV1 from "./Template/Natal/NatalV1";
+import BottomNavbar from "@/Components/invitation/BottomNavbar";
 
 export default function Invitation() {
     return (
         <div className="w-full relative">
-            {/* <KhitanV1 /> */}
             {/* <WeddingV1 /> */}
             <NatalV1 />
             <BottomNavbar />
--- routes/web.php
+++ routes/web.php
@@ -37,14 +37,9 @@ Route::get('/dashboard/wedding/edit/content', function () {
     return Inertia::render('Invitation/Template/Wedding/Content/ContentWeddingV1');
 });
 
-Route::get('/dashboard/khitan/edit/content', function () {
-    return Inertia::render('Invitation/Template/Khitan/Content/ContentKhitanV1');
-});
-
 Route::get('/dashboard/natal/edit/content', function () {
     return Inertia::render('Invitation/Template/Natal/Content/ContentNatalV1');
 });
-
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard/orders', function () {
